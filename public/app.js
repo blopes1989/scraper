3f26ce1cea5ac82ad647f43c14c751036af706f8
bizzy <blopes1989@gmail.com>
2018-10-09 01:34:38 -0400

    dynmamic

---
+++ public/app.js
@@ -0,0 +1,56 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#base").append(
+        "<div class='card cardOuter'>" +
+        "<div class='row'>" +
+        "<div class='card-body col-8'>" +
+        "<h5 class='card-title'> </h5>" +
+        "<p class='card-text'>With supporting text below as a natural lead-in to additional content.</p>" +
+        "</div>"+
+        "<div class='card cardInner'>"+
+        "<h5 class='card-header'>Leave a comment!</h5>"+
+        "<div class='card-body col'>"+
+        "<input type='text' class='form-control' placeholder='First name'>"+
+        "<a href='#' class='btn btn-primary id= 'submit'>Post</a>"+
+        "</div>"+
+        "</div>"+
+        "</div>"+
+        "</div>"
+      
+      );
+    }
+  });
+  
+//   // When you click the submit button
+   $(document).on("click", "#submit", function() {
+//     // Grab the id associated with the article from the submit button
+     var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+   });
+  
