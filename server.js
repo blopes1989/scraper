40d4c98ce3803986449854306ae0941c8a4ef9c3
bizzy <blopes1989@gmail.com>
2018-10-10 21:01:02 -0400

    https

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://gamespot.com").then(function(response) {
+  axios.get("https://gamespot.com").then(function(response) {
 
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
